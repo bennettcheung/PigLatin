282dc308ae77fa7db0fd834c8e331b3d82dcecf4
Bennett <bennettcheung@yahoo.com>
2018-08-03 14:19:01 -0700

    Initial Commit

---
+++ PigLatin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PigLatin
+//
+//  Created by Bennett on 2018-08-03.
+//  Copyright © 2018 Bennett. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
